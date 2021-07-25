07ff26a0cc3b682cfc6eb28323682a828a898c2d
Meerza Ahmed <36519315+MeerzaA@users.noreply.github.com>
2021-07-24 20:02:53 -0400

    Add files via upload

---
+++ HW Managment System/Capstone Project Data Structures/Capstone Project Data Structures.cpp
@@ -0,0 +1,21 @@
+// Author : Meerza
+// Goal : To build homework managment system 
+// BCS 370 Capstone Project
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#include "hwManagment.h"
+#include <time.h>
+
+
+int main()
+{
+	Capstone cap;
+	cap.startProject();
+	return 0;
+}
+
